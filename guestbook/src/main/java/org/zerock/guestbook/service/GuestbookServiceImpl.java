70193e2d545a3b1f9f185a050fafe63d8f46bfb2
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-07-11 22:50:02 +0900

    07.11

--- guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -1,5 +1,7 @@
 package org.zerock.guestbook.service;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
@@ -10,6 +12,7 @@ import org.zerock.guestbook.dto.GuestbookDTO;
 import org.zerock.guestbook.dto.PageRequestDTO;
 import org.zerock.guestbook.dto.PageResultDTO;
 import org.zerock.guestbook.entity.Guestbook;
+import org.zerock.guestbook.entity.QGuestbook;
 import org.zerock.guestbook.repository.GuestbookRepository;
 
 import java.util.Optional;
@@ -43,12 +46,17 @@ public class GuestbookServiceImpl implements GuestbookService{
         return entity.getGno();
     }
 
+    // 목록 조회
     @Override
     public PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO) {
 
         Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
+        
+        // 검색 조건 처리
+        BooleanBuilder booleanBuilder = getSearch(requestDTO);
 
-        Page<Guestbook> result = repository.findAll(pageable);
+        // booleanBuilder 추가하여 Querydsl 사용
+        Page<Guestbook> result = repository.findAll(booleanBuilder,pageable);
 
         // entityToDto()를 이용해 Function생성하고 이를 PageResultDTO로 구성
         Function<Guestbook, GuestbookDTO> fn = (entity -> entityToDto(entity));
@@ -87,6 +95,49 @@ public class GuestbookServiceImpl implements GuestbookService{
 
             repository.save(entity);
         }
+    }
+    
+    // PageRequestDTO를 파라미터로 받아 검색조건(type)이 있는 경우 conditionBuilder 변수를 생성해 각 검색조건을 or로 연결해 처리
+    // 검색조건이 없다면 gno > 0 으로만 생성
+    private BooleanBuilder getSearch(PageRequestDTO requestDTO){
+        //Querydsl 처리
+        String type = requestDTO.getType();
+        
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+        
+        String keyword = requestDTO.getKeyword();
+
+        // gno > 0 조건만 생성
+        BooleanExpression expression = qGuestbook.gno.gt(0L);
+        
+        booleanBuilder.and(expression);
+        
+        // 검색 조건이 없는경우
+        if(type == null || type.trim().length() == 0){
+            return booleanBuilder;
+        }
+        
+        // 검색 조건 작성
+        
+        BooleanBuilder conditionBuilder = new BooleanBuilder();
+        
+        if(type.contains("t")){
+            conditionBuilder.or(qGuestbook.title.contains(keyword));
+        }
+        if(type.contains("c")){
+            conditionBuilder.or(qGuestbook.content.contains(keyword));
+        }
+        if(type.contains("w")){
+            conditionBuilder.or(qGuestbook.writer.contains(keyword));
+        }
+        
+        // 모든 조건 통합
+        booleanBuilder.and(conditionBuilder);
+        
+        return booleanBuilder;
+        
     }
 
 
