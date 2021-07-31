594c7768087b80ac04c27cf44cb9bfe05f2255ae
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-07-31 13:21:16 +0900

    07.31

--- board/board/src/main/java/org/zerock/board/service/ReplyService.java
+++ board/board/src/main/java/org/zerock/board/service/ReplyService.java
@@ -31,6 +31,21 @@ public interface ReplyService {
                 .replyer(replyDTO.getReplyer())
                 .board(board)
                 .build();
+
+        return reply;
+    }
+
+    // Reply객체를 ReplyDTO로 변환 Board 객체가 필요하지 않으므로 게시물 번호만
+    default ReplyDTO entityToDTO(Reply reply){
+        ReplyDTO dto = ReplyDTO.builder()
+                .rno(reply.getRno())
+                .text(reply.getText())
+                .replyer(reply.getReplyer())
+                .regDate(reply.getRegDate())
+                .modDate(reply.getModDate())
+                .build();
+
+        return dto;
     }
 
 }
