2377e1af0e414040627b84d931405b422b386ae2
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-08-05 06:17:22 +0900

    08.05

--- mreview/mreview/src/test/java/org/zerock/mreview/repository/MovieRepositoryTests.java
+++ mreview/mreview/src/test/java/org/zerock/mreview/repository/MovieRepositoryTests.java
@@ -12,6 +12,7 @@ import org.zerock.mreview.entity.Movie;
 import org.zerock.mreview.entity.MovieImage;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 import java.util.stream.IntStream;
 
@@ -67,6 +68,19 @@ public class MovieRepositoryTests {
 
         for(Object[] objects : result.getContent()) {
             System.out.println(Arrays.toString(objects));
+        }
+    }
+
+    // 특정 영화의 모든 이미지와 평균 평점/리뷰 개수 조회
+    @Test
+    public void testGetMovieWithAll(){
+
+        List<Object[]> result = movieRepository.getMovieWithAll(94L);
+
+        System.out.println(result);
+
+        for(Object[] arr : result) {
+            System.out.println(Arrays.toString(arr));
         }
     }
 
