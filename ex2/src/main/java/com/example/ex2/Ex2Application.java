29886053c081637a88221103472ba9bb50882f5d
qkralswl689@gmail.com <qkralswl689@gmail.com>
2022-01-25 22:44:43 +0900

    DB연결 및 어노테이션 설명

--- ex2/src/main/java/com/example/ex2/Ex2Application.java
+++ ex2/src/main/java/com/example/ex2/Ex2Application.java
@@ -2,8 +2,12 @@ package com.example.ex2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.example")
+
+
 public class Ex2Application {
 
 	public static void main(String[] args) {
