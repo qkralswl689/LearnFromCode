93fb7b2136e61e87c7288a562afea3edb3ac710b
qkralswl689@gmail.com <qkralswl689@gmail.com>
2022-01-25 19:43:22 +0900

    second plus jpa

---
+++ ex2/src/main/java/com/example/entity/Memo.java
@@ -0,0 +1,22 @@
+package com.example.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tbl_memo")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Memo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mno;
+
+    @Column
+    private String memoText;
+
+}
