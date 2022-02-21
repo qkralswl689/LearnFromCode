a254ac46cfc79dba04bb8957a6d3233b1bb52cd2
qkralswl689@gmail.com <qkralswl689@gmail.com>
2022-02-21 22:42:00 +0900

    Q엔티티추가

--- board2022/src/main/java/com/example/board2022/repository/search/SearchBoardRepositoryImpl.java
+++ board2022/src/main/java/com/example/board2022/repository/search/SearchBoardRepositoryImpl.java
@@ -1,10 +1,13 @@
 package com.example.board2022.repository.search;
 
 import com.example.board2022.entity.Board;
-import lombok.extern.log4j.Log4j2;
+import com.example.board2022.entity.QBoard;
+import com.querydsl.jpa.JPQLQuery;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
-@Log4j2
+import java.util.List;
+
+
 public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport implements SearchBoardRepository {
 
     public SearchBoardRepositoryImpl() { // QuerydslRepositorySupport는 생성자가 존재하므로 클래스 내에서 super()를 이용해 호출해야 한다
@@ -14,7 +17,15 @@ public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport impleme
     @Override
     public Board search1() {
 
-        log.info("search1............");
+        // log.info("search1............");
+
+        QBoard board = QBoard.board;
+
+        JPQLQuery<Board> jpqlQuery = from(board);
+
+        jpqlQuery.select(board).where(board.bno.eq(1L));
+
+        List<Board> result = jpqlQuery.fetch();
 
         return null;
     }
