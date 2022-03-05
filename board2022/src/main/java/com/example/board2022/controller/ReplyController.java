41dda99b85a00e5fa82f1b817f0621c78b82415b
qkralswl689@gmail.com <qkralswl689@gmail.com>
2022-03-05 17:14:40 +0900

    댓글저장,수정,삭제

--- board2022/src/main/java/com/example/board2022/controller/ReplyController.java
+++ board2022/src/main/java/com/example/board2022/controller/ReplyController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +23,22 @@ public class ReplyController {
     public ResponseEntity<List<ReplyDTO>> getListByBoard(@PathVariable("bno") Long bno){
 
         return new ResponseEntity<>(replyService.getList(bno), HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    // @RequestBody  : JSON으로 들어오는 데이터를 자동으로 해당 타입의 객체로 매핑해주는 역할 -> 별도의 처리 없이도 JSON 데이터를 특정 타입의 객체로 변환해서 처리 가능
+    public ResponseEntity<Long> register(@RequestBody ReplyDTO replyDTO){
+
+        Long rno = replyService.register(replyDTO);
+
+        return new ResponseEntity<>(rno, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{rno}")
+    public ResponseEntity<String> remove(@PathVariable("rno") Long rno){
+
+        replyService.remove(rno);
+
+        return new ResponseEntity<>("success", HttpStatus.OK);
     }
 }
