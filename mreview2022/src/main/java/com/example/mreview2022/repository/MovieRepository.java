80bab04848899118c4681aa7654f63b3e94c6fe0
qkralswl689@gmail.com <qkralswl689@gmail.com>
2022-03-07 21:39:14 +0900

    쿼리추가

--- mreview2022/src/main/java/com/example/mreview2022/repository/MovieRepository.java
+++ mreview2022/src/main/java/com/example/mreview2022/repository/MovieRepository.java
@@ -8,8 +8,13 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface MovieRepository extends JpaRepository<Movie,Long> {
 
-    @Query("select m, avg(coalesce(r.grade,0)), count(distinct r) from Movie m "
-            + "left outer join Review r on r.movie = m group by m")
+    //    @Query("select m, avg(coalesce(r.grade,0)),  count(r) from Movie m " +
+    //            "left outer join Review  r on r.movie = m group by m")
+    //    Page<Object[]> getListPage(Pageable pageable);
+
+    @Query("select m, mi, avg(coalesce(r.grade,0)),  count(distinct r) from Movie m " +
+            "left outer join MovieImage mi on mi.movie = m " +
+            "left outer join Review r on r.movie = m group by m ")
     Page<Object[]> getListPage(Pageable pageable);
 
 }
