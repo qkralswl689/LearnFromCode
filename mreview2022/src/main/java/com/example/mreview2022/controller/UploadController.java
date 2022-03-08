8a852afd38b71baf7cab6890c076230f626d4ecc
qkralswl689@gmail.com <qkralswl689@gmail.com>
2022-03-08 17:37:08 +0900

    파일업로드 및 이미지 출력

---
+++ mreview2022/src/main/java/com/example/mreview2022/controller/UploadController.java
@@ -0,0 +1,110 @@
+package com.example.mreview2022.controller;
+
+import com.example.mreview2022.dto.UploadResultDTO;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+public class UploadController {
+
+    @Value("${com.example.upload.path}") // application.properties의 변수
+    private String uploadPath;
+
+    @PostMapping("/uploadAjax")
+    public ResponseEntity<List<UploadResultDTO>> uploadFile(MultipartFile[] uploadFiles){
+
+        List<UploadResultDTO> resultDTOList = new ArrayList<>();
+        for (MultipartFile uploadFile : uploadFiles) {
+
+            // 이미지 파일만 업로드 가능
+            if(uploadFile.getContentType().startsWith("image") == false){
+                // 이미지가 아닌경우 403 Forbidden 반환
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+            }
+
+            // 실제 파일 이름 IE나 Edge는 전체 경로가 들어오므로
+            String originalName = uploadFile.getOriginalFilename();
+
+            String fileName = originalName.substring(originalName.lastIndexOf("\\") + 1);
+
+            // 날짜 폴더 생성
+            String folderPath = makeFolder();
+
+            //UUID
+            String uuid = UUID.randomUUID().toString();
+
+            //저장할 파일 이름
+            String saveName = uploadPath + File.separator + folderPath + File.separator + uuid + fileName;
+
+            Path savePath = Paths.get(saveName);
+
+            try {
+                uploadFile.transferTo(savePath);// 실제 이미지 저장
+                resultDTOList.add(new UploadResultDTO(fileName,uuid,folderPath));
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
+        return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
+    }
+
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(String fileName){
+
+        ResponseEntity<byte[]> result = null;
+
+        try {
+
+            String srcFileName = URLDecoder.decode(fileName,"UTF-8");
+
+            File file = new File(uploadPath + File.separator + srcFileName);
+
+            HttpHeaders header = new HttpHeaders();
+
+            //MIME타입 처리
+            header.add("Content-Type", Files.probeContentType(file.toPath()));
+
+            //파일 데이터 처리
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+        }catch (Exception e){
+
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return result;
+    }
+    private String makeFolder() {
+
+        String str = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+
+        String folderPath = str.replace("/", File.separator);
+
+        // make folder ----
+        File uploadPatheFolder = new File(uploadPath,folderPath);
+
+        if(uploadPatheFolder.exists() == false){
+            uploadPatheFolder.mkdirs();
+        }
+
+        return folderPath;
+    }
+}
