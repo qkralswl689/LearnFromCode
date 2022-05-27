7c5760db2e31b9cbaea3738cbf49b21b8b5ba45b
qkralswl689@gmail.com <qkralswl689@gmail.com>
2022-05-27 17:46:50 +0900

    시큐리치 커스터마이징

--- club/src/main/java/com/example/club/config/SecurityConfig.java
+++ club/src/main/java/com/example/club/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.example.club.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,5 +16,27 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                // 모든사용자 접근가능
+                .antMatchers("/sample/all").permitAll()
+                // USER 라는 권한이 있는 사용자만 접근가능
+               .antMatchers("/sample/member").hasRole("USER");
 
+        // 인가/인증 문제시 로그인 화면
+        http.formLogin();
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // 사용자 계멍 : user1
+        auth.inMemoryAuthentication().withUser("user1")
+        // 1111 pw 인코딩 결과
+        .password("$2a$10$ORfLbDWdDks2osZmIcnHu.X/aH.DxoJVI9tq.3Vw0pjGtSlr/Pzcq")
+        // 권한
+        .roles("USER");
+    }
 }
