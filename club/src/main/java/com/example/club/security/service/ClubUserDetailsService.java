bd023960b2326f233c9caf5e5366b21ad33a16e4
qkralswl689@gmail.com <qkralswl689@gmail.com>
2022-06-02 19:22:35 +0900

    clubMemeberRepository 연동

--- club/src/main/java/com/example/club/security/service/ClubUserDetailsService.java
+++ club/src/main/java/com/example/club/security/service/ClubUserDetailsService.java
@@ -1,17 +1,51 @@
 package com.example.club.security.service;
 
+import com.example.club.entity.ClubMember;
+import com.example.club.repository.ClubMemberRepository;
+import com.example.club.security.dto.ClubAuthMemberDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
+@RequiredArgsConstructor
 public class ClubUserDetailsService implements UserDetailsService {
+
+    // ClubMemberRepository 주입
+    private final ClubMemberRepository clubMemberRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        System.out.println("ClubUserDetailsService loadUserByUsername " + username);
-        return null;
+        Optional<ClubMember> result = clubMemberRepository.findByEmail(username,false);
+
+        // 사용자 존재하지 않을경우
+        if(result.isPresent()){
+            throw new UsernameNotFoundException("Check Email or Social");
+        }
+
+        ClubMember clubMember = result.get();
+
+        // ClubAuthMember 를 UserDetails 차입으로 처리하기위해 ClubAuthMemberDTO 타입으로 변환
+        ClubAuthMemberDTO clubAuthMember = new ClubAuthMemberDTO(
+                clubMember.getEmail(),
+                clubMember.getPassword(),
+                clubMember.isFormSocial(),
+                clubMember.getRoleSet().stream()
+                        .map(role -> new SimpleGrantedAuthority
+                                ("ROLE" + role.name())).collect(Collectors.toSet())
+        );
+
+        clubAuthMember.setName(clubAuthMember.getName());
+        clubAuthMember.setFromSocial(clubAuthMember.isFromSocial());
+
+       // System.out.println("ClubUserDetailsService loadUserByUsername " + username);
+        return clubAuthMember;
     }
 }
